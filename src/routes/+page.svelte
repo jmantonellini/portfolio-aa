ca63986c446faf263a8df7510e14442a38d404ff
Juan Manuel Antonellini <juanmantonellini@gmail.com>
2026-05-31 00:52:39 -0300

    Assure video play

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,20 @@
 	import { m } from '$lib/paraglide/messages';
 	import videoSrc from '$lib/assets/depto-loop.webm';
 	import NavBar from '$lib/components/NavBar.svelte';
+	import { onMount } from 'svelte';
+
+	let videoElement = $state();
+	let videoUrl = $state('');
+
+	onMount(() => {
+		// Asegura que el video se cargue después de la hidratación
+		videoUrl = videoSrc;
+
+		// Forzar reproducción si falla autoplay
+		if (videoElement) {
+			videoElement.play().catch((e) => console.log('Autoplay prevented:', e));
+		}
+	});
 </script>
 
 <div
@@ -12,7 +26,16 @@
 	<div class="absolute bottom-20 z-20 w-screen px-10 text-2xl lg:px-40 lg:text-end">
 		{m.silly_stock_whale_lock()}
 	</div>
-	<video class="absolute w-2xl z-10 translate-x-5 max-w-none object-cover lg:w-full" src={videoSrc} autoplay muted loop></video>
+	{#if videoUrl}
+		<video
+			bind:this={videoElement}
+			class="absolute z-10 w-2xl max-w-none translate-x-5 object-cover lg:w-full"
+			src={videoUrl}
+			autoplay
+			muted
+			loop
+		></video>
+	{/if}
 	<!-- <Canvas>
 			<Scene />‰
 		</Canvas> -->
